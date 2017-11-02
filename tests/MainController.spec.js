8bad2c81c6b769d71b4c1e7990e13692c9b91991
Mike <michael.ries@gmail.com>
2017-11-01 20:35:01 -0500

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,10 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
-    });
+describe("Basic Functionality", function() {
+  it("should add two numbers", function() {
+    expect(addNumbers(1, 3)).toBe(4);
+    expect(addNumbers(343, 9283)).toBe(9626);
+    expect(addNumbers(1223, 21)).toBe(1244);
+    expect(addNumbers(10, 653)).toBe(663);
+    expect(addNumbers("99", 1)).toBe(100);
+    expect(addNumbers("23", "59")).toBe(82);
+  });
 });
